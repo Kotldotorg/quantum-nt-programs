4147ceab360befafc8c9bd53503932eeb2007147
Kotl <130578244+Kotldotorg@users.noreply.github.com>
2023-08-08 19:46:36 +0300

    Fixed TaskManager

---
+++ Taskmgr/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Taskmgr
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        void GetAllProcess()
+        {
+            proc = Process.GetProcesses();
+            listBox1.Items.Clear();
+            foreach (Process p in proc)
+                listBox1.Items.Add(p.ProcessName);
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GetAllProcess();
+        }
+        Process[] proc;
+        private object listBox;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            { 
+                proc[listBox1.SelectedIndex].Kill();
+                GetAllProcess();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+        private void runNewTaskToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using(frmRunTask frm = new frmRunTask())
+            {
+                if(frm.ShowDialog() == DialogResult.OK)
+                    GetAllProcess();
+            }
+
+        }
+
+        private void shutdownToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = "/C shutdown /s";
+            process.StartInfo = startInfo;
+            process.Start();
+        }
+
+        private void restartToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = "/C shutdown /r";
+            process.StartInfo = startInfo;
+            process.Start();
+        }
+
+        private void sleepToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = "/C rundll32.exe powrprof.dll, SetSuspendState Sleep";
+            process.StartInfo = startInfo;
+            process.Start();
+        }
+
+        private void logoffToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = "/C shutdown /l";
+            process.StartInfo = startInfo;
+            process.Start();
+        }
+    }
+}
